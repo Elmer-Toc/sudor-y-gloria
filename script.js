b1928f5bbee07be73600b3b3bbf6ef214e951c3d
Elmer-Toc <etoct2@miumg.edu.gt>
2026-05-03 09:53:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const productos = [
         descripcion: 'Mangas cortas, tejido ligero que absorbe sudor. Ideal para running y pesas.',
         precio: 89.00,
         imagen: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=600&h=800&fit=crop',
-        colores: ['Negro', 'Gris', 'Azul Marino'],
+        colores: ['Negro', 'Gris', 'Azul Marino','blanco'],
         tallas: ['S', 'M', 'L', 'XL', 'XXL'],
         categoria: 'hombres',
         badge: 'Nuevo'
